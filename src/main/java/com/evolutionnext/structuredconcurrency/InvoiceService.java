5d9f0eead0c8254541ad992779d9262cc381940a
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-11-10 19:24:58 +0000

    Better block organization for InvoiceService

--- src/main/java/com/evolutionnext/structuredconcurrency/InvoiceService.java
+++ src/main/java/com/evolutionnext/structuredconcurrency/InvoiceService.java
@@ -6,24 +6,23 @@ public class InvoiceService {
     public List<Invoice> findAllInvoicesByUser(Long id) {
         System.out.println("findAllInvoicesByUser: " + Thread.currentThread());
         return List.of(
-            new Invoice("402", 1120.00F),
-            new Invoice("1402", 1220.00F),
+                new Invoice("402", 1120.00F),
+                new Invoice("1402", 1220.00F),
             new Invoice("671", 1220.00F)
         );
     }
 
     public List<Invoice> findAllInvoicesByUserLongTime(long id) {
-        System.out.println("findAllInvoicesByUser" + Thread.currentThread());
+        System.out.println("findAllInvoicesByUserLongTime" + Thread.currentThread());
         try {
             Thread.sleep(3000);
+            return List.of(
+                    new Invoice("402", 1120.00F),
+                    new Invoice("1402", 1220.00F),
+                    new Invoice("671", 1220.00F));
         } catch (InterruptedException e) {
             System.out.println("This thread was interrupted");
             throw new RuntimeException(e);
         }
-        return List.of(
-            new Invoice("402", 1120.00F),
-            new Invoice("1402", 1220.00F),
-            new Invoice("671", 1220.00F)
-        );
     }
 }
